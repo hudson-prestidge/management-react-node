3defc93f7d2f2cfded6c5c59ecba460465d12734
Hudson Prestidge <charcon@ihug.co.nz>
2017-09-07 17:15:35 +1200

    basic express server setup

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
-console.log('test');
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const PORT = 3000
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/../client/index.html')))
+
+app.listen(PORT, () => console.log(`Server is running and listening on port ${PORT}.`))
