3580ef4d4d5a1181c9224817927c874419c63400
Hudson Prestidge <charcon@ihug.co.nz>
2017-09-08 15:47:13 +1200

    bare bones react implementation

---
+++ client/app.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class App extends React.Component {
+
+  render() {
+    console.log('rendering app component');
+    return (
+      <div>
+        Simple component
+      </div>
+    )
+  }
+
+}
+
+export default App
